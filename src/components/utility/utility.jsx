03ec8cfc94ce694df7ecd7221224e4983a3c50fa
Brian Bouchard <b.bouch@gmail.com>
2020-03-28 21:58:46 -0600

    add dropdown for chart type

--- src/components/utility/utility.jsx
+++ src/components/utility/utility.jsx
@@ -4,7 +4,10 @@ import './utility.scss';
 function Utility(props) {
   return (
     <div>
-
+      <label for="chartType">Choose a chart type:</label>
+      <select id="chartType">
+        <option value="bar">bar</option>
+      </select>
     </div>
   )
 }
