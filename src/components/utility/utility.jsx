2b9d00fdb6db3bcd644bbeff6ba7015e9a815581
Brian Bouchard <b.bouch@gmail.com>
2020-03-30 12:29:14 -0600

    style inputs

--- src/components/utility/utility.jsx
+++ src/components/utility/utility.jsx
@@ -5,22 +5,22 @@ import 'bootstrap/dist/css/bootstrap.css'
 function Utility(props) {
   return (
     <form>
-      <div class="input-group mb-3" id="chart-type-input">
-        <div class="input-group-prepend">
-          <label class="input-group-text" for="inputGroupSelect01">Chart Type</label>
+      <div className="input-container">
+        <div class="input-group input-group-sm mb-5" id="chart-type-input">
+          <div class="input-group-prepend">
+            <label class="input-group-text col-form-label-sm" for="inputGroupSelect01" id="inputGroup-sizing-sm">Chart Type</label>
+          </div>
+          <select class="form-control form-control-sm" id="inputGroupSelect01">
+            <option selected>Choose...</option>
+            <option value="Bar">Bar</option>
+          </select>
         </div>
-        <select class="custom-select" id="inputGroupSelect01">
-          <option selected>Choose...</option>
-          <option value="Bar">Bar</option>
-        </select>
-      </div>
-      <div class="input-group" id="date-input">
-        <div class="input-group-prepend">
-          <span class="input-group-text">Select Dates</span>
-        </div>
-        <div>
-          <input type="date" aria-label="First name" class="form-control" id="start-date-input"/>
-          <input type="date" aria-label="Last name" class="form-control" id="end-date-input"/>
+        <div class="input-group input-group-sm mb-5" id="chart-type-input">
+          <div class="input-group-prepend">
+            <span class="input-group-text col-form-label-sm" id="inputGroup-sizing-sm">Start and End Dates</span>
+          </div>
+          <input type="date" class="form-control col-form-label-sm" id="inputGroup-sizing-sm"/>
+          <input type="date" class="form-control col-form-label-sm" id="inputGroup-sizing-sm"/>
         </div>
       </div>
     </form>
