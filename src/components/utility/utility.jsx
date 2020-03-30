770741be88eded0fdc874f41b7417e7fce4c69ef
Brian Bouchard <b.bouch@gmail.com>
2020-03-29 23:51:59 -0600

    style inputs

--- src/components/utility/utility.jsx
+++ src/components/utility/utility.jsx
@@ -5,24 +5,22 @@ import 'bootstrap/dist/css/bootstrap.css'
 function Utility(props) {
   return (
     <form>
-      <div className="input-container">
-        <div class="input-group mb-3" id="chart-type-input">
-          <div class="input-group-prepend">
-            <label class="input-group-text" for="inputGroupSelect01">Chart Type</label>
-          </div>
-          <select class="custom-select" id="inputGroupSelect01">
-            <option selected>Choose...</option>
-            <option value="Bar">Bar</option>
-          </select>
+      <div class="input-group mb-3" id="chart-type-input">
+        <div class="input-group-prepend">
+          <label class="input-group-text" for="inputGroupSelect01">Chart Type</label>
         </div>
-        <div class="input-group" id="date-input">
-          <div class="input-group-prepend">
-            <span class="input-group-text">Select Dates</span>
-          </div>
-          <div>
-            <input type="date" aria-label="First name" class="form-control" id="start-date-input"/>
-            <input type="date" aria-label="Last name" class="form-control" id="end-date-input"/>
-          </div>
+        <select class="custom-select" id="inputGroupSelect01">
+          <option selected>Choose...</option>
+          <option value="Bar">Bar</option>
+        </select>
+      </div>
+      <div class="input-group" id="date-input">
+        <div class="input-group-prepend">
+          <span class="input-group-text">Select Dates</span>
+        </div>
+        <div>
+          <input type="date" aria-label="First name" class="form-control" id="start-date-input"/>
+          <input type="date" aria-label="Last name" class="form-control" id="end-date-input"/>
         </div>
       </div>
     </form>
