ab7768d28978c9af80d0db139526a3ea39cab040
Brian Bouchard <b.bouch@gmail.com>
2020-03-30 14:10:04 -0600

    change page title color and add media query to shrink inputs based on 500px screen size

--- src/components/utility/utility.jsx
+++ src/components/utility/utility.jsx
@@ -6,11 +6,11 @@ function Utility(props) {
   return (
     <form>
       <div className="input-container">
-        <div class="input-group input-group-sm" id="chart-type-input">
+        <div class="input-group input-group-sm" id="chart-type-input-01">
           <div class="input-group-prepend">
             <label class="input-group-text" for="inputGroupSelect01" id="inputGroup-sizing-sm">Chart Type</label>
           </div>
-          <select class="form-control" id="inputGroupSelect01">
+          <select class="form-control " id="inputGroupSelect01">
             <option selected>Choose...</option>
             <option value="Bar" selected="Bar">Bar</option>
           </select>
